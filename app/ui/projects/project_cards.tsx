0ce2ae73937437eb848605a3bc43406736de1a59
Sean <sdillon215@gmail.com>
2023-12-28 16:37:17 -0800

    fix(project_card): duh add key to the Card component

--- app/ui/projects/project_cards.tsx
+++ app/ui/projects/project_cards.tsx
@@ -7,19 +7,21 @@ export default async function ProjectCards() {
   return (
     <>
       {projectData.map(((project) =>
-        <Card key={project.id} project={project} />))}
+        <Card projKey={project.id} project={project} />))}
     </>
   );
 }
 
 export function Card({
+  projKey,
   project
 }: {
+  projKey: string;
   project: Project;
 }) {
 
   return (
-    <Link href={`/projects/${project.id}`}>
+    <Link key={projKey} href={`/projects/${project.id}`}>
       <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
         <div className="flex p-4">
           {/* {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null} */}
