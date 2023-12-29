560b83acc1acae7c2da4e8cea8ede2dfcd38a1d7
Sean <sdillon215@gmail.com>
2023-12-28 16:56:22 -0800

    revert-ish oops

--- app/ui/projects/project_cards.tsx
+++ app/ui/projects/project_cards.tsx
@@ -7,21 +7,19 @@ export default async function ProjectCards() {
   return (
     <>
       {projectData.map(((project) =>
-        <Card projKey={project.id} project={project} />))}
+        <Card key={project.id} project={project} />))}
     </>
   );
 }
 
 export function Card({
-  projKey,
   project
 }: {
-  projKey: string;
   project: Project;
 }) {
 
   return (
-    <Link key={projKey} href={`/projects/${project.id}`}>
+    <Link href={`/projects/${project.id}`}>
       <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
         <div className="flex p-4">
           {/* {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null} */}
